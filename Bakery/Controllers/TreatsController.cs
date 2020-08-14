c8e5d5a92a49dea78346e262bb753d376ffe39f8
Noel Kirkland <noelkirkland@gmail.com>
2020-08-14 13:51:06 -0700

    create TreatsController Details route

--- Bakery/Controllers/TreatsController.cs
+++ Bakery/Controllers/TreatsController.cs
@@ -48,5 +48,14 @@ namespace Bakery.Controllers
       _db.SaveChanges();
       return RedirectToAction("Index");
     }
+
+    public ActionResult Details(int id)
+    {
+      Treat thisTreat = _db.Treats 
+        .Include(treat => treat.Flavors) 
+        .ThenInclude(join => join.Flavor) 
+        .FirstOrDefault(treat => treat.TreatId == id); 
+      return View(thisTreat);
+    }
   }
 }
