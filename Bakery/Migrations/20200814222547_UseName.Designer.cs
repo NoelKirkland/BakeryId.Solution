5a3bf52f2f371de6202f4091d3b82f3d432212cf
Noel Kirkland <noelkirkland@gmail.com>
2020-08-14 15:58:07 -0700

    add a delete button to Details view

--- Bakery/Migrations/20200814222547_UseName.Designer.cs
+++ Bakery/Migrations/20200814222547_UseName.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using Bakery.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Bakery.Migrations
 {
     [DbContext(typeof(BakeryContext))]
-    partial class BakeryContextModelSnapshot : ModelSnapshot
+    [Migration("20200814222547_UseName")]
+    partial class UseName
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Bakery/Migrations/20200814222547_UseName.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Bakery.Migrations
+{
+    public partial class UseName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UserId",
+                table: "Treats",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserId",
+                table: "Flavors",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Treats_UserId",
+                table: "Treats",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Flavors_UserId",
+                table: "Flavors",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Flavors_AspNetUsers_UserId",
+                table: "Flavors",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Treats_AspNetUsers_UserId",
+                table: "Treats",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Flavors_AspNetUsers_UserId",
+                table: "Flavors");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Treats_AspNetUsers_UserId",
+                table: "Treats");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Treats_UserId",
+                table: "Treats");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Flavors_UserId",
+                table: "Flavors");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Treats");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Flavors");
+        }
+    }
+}
